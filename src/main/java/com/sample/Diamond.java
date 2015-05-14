bf9dd84235fe6b117b76a311c8b604619cb4a341
Florian MORGAN <florian.morgan@gmail.com>
2015-05-14 08:15:06 +0200

    A, B, ...

--- src/main/java/com/sample/Diamond.java
+++ src/main/java/com/sample/Diamond.java
@@ -1,7 +1,33 @@
 package com.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diamond {
+
+    private static final String A = "A";
+    private static final String B = "B";
+    private static final String SPACE = " ";
+
     public static String draw(String character) {
-        throw new UnsupportedOperationException("draw");
+        List<String> lines = new ArrayList<>();
+        int charNum = character.charAt(0) - 'A';
+        String line1;
+        if (charNum == 0) {
+            line1 = A;
+            lines.add(line1);
+        }
+        if (charNum == 1) {
+            line1 = SPACE + A;
+            lines.add(line1);
+            lines.add(B + SPACE + B);
+            lines.add(line1);
+        }
+        if (charNum == 2) {
+            line1 = SPACE + SPACE + A;
+            lines.add(line1);
+            lines.add(SPACE + B + SPACE + B);
+        }
+        return String.join("\n", lines);
     }
 }
--- src/test/java/com/sample/DiamondTest.java
+++ src/test/java/com/sample/DiamondTest.java
@@ -12,4 +12,24 @@ public class DiamondTest {
         assertThat(Diamond.draw("A"), is("A"));
     }
 
+    @Test public void testShouldReturn_AForFirstLineWhenGivenB() {
+        assertThat(Diamond.draw("B").split("\n")[0], is(" A"));
+    }
+
+    @Test public void testShouldReturnB_BForSecondLineWhenGivenB() {
+        assertThat(Diamond.draw("B").split("\n")[1], is("B B"));
+    }
+
+    @Test public void testShouldReturn_AForThirdLineWhenGivenB() {
+        assertThat(Diamond.draw("B").split("\n")[2], is(" A"));
+    }
+
+    @Test public void testShouldReturn__AForFirstLineWhenGivenC() {
+        assertThat(Diamond.draw("C").split("\n")[0], is("  A"));
+    }
+
+    @Test public void testShouldReturn_B_BForSecondLineWhenGivenC() {
+        assertThat(Diamond.draw("C").split("\n")[1], is(" B B"));
+    }
+
 }
